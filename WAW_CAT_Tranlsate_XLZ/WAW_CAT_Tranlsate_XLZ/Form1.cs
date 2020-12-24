d199d8b2cd288abfdea7402e03a041b040070d21
alekparol92@gmail.com <40210140+alekparol@users.noreply.github.com>
2020-12-24 10:06:11 +0100

    Creating Form

---
+++ WAW_CAT_Tranlsate_XLZ/WAW_CAT_Tranlsate_XLZ/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WAW_CAT_Tranlsate_XLZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
